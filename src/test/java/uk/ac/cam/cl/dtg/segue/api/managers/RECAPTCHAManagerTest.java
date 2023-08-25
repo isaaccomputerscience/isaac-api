763e1ac5aefe7a9cd26222e0b00eab4c020b7a04
Kasim Ali <kasim.ali@and.digital>
2023-08-25 15:36:11 +0100

    Removed some unnecessary throws in the tests for recaptcha

--- src/test/java/uk/ac/cam/cl/dtg/segue/api/managers/RECAPTCHAManagerTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/segue/api/managers/RECAPTCHAManagerTest.java
@@ -38,39 +38,39 @@ public class RECAPTCHAManagerTest {
     }
 
     @Test
-    public final void verifyRecaptcha_ValidResponse_ShouldReturnSuccess() throws Exception {
+    public final void verifyRecaptcha_ValidResponse_ShouldReturnSuccess() {
         RECAPTCHAManager testInstance = createTestInstance(SUCCESS_RESPONSE_JSON);
         assertTrue(testInstance.verifyRecaptcha("valid-response"));
     }
 
     @Test
-    public final void isCaptchaValid_ValidResponse_ShouldReturnSuccessMessage() throws Exception {
+    public final void isCaptchaValid_ValidResponse_ShouldReturnSuccessMessage() {
         RECAPTCHAManager testInstance = createTestInstance(SUCCESS_RESPONSE_JSON);
         assertEquals("reCAPTCHA verification successful.", testInstance.recaptchaResultString("valid-response"));
     }
 
     @Test
-    public final void verifyRecaptcha_InvalidResponse_ShouldReturnFailure() throws Exception {
+    public final void verifyRecaptcha_InvalidResponse_ShouldReturnFailure() {
         setSecretInvalid();
         RECAPTCHAManager testInstance = createTestInstance(FAILURE_RESPONSE_JSON);
         assertFalse(testInstance.verifyRecaptcha("invalid-response"));
     }
 
     @Test
-    public final void isCaptchaValid_InvalidResponse_ShouldReturnFailedMessage() throws Exception {
+    public final void isCaptchaValid_InvalidResponse_ShouldReturnFailedMessage() {
         setSecretInvalid();
         RECAPTCHAManager testInstance = createTestInstance(FAILURE_RESPONSE_JSON);
         assertEquals("reCAPTCHA verification failed.", testInstance.recaptchaResultString("invalid-response"));
     }
 
     @Test
-    public final void verifyRecaptcha_EmptyResponse_ShouldReturnFailure() throws Exception {
+    public final void verifyRecaptcha_EmptyResponse_ShouldReturnFailure() {
         RECAPTCHAManager testInstance = createTestInstance(FAILURE_RESPONSE_JSON);
         assertFalse(testInstance.verifyRecaptcha(""));
     }
 
     @Test
-    public final void isCaptchaValid_EmptyResponse_ShouldReturnMissingResponseMessage() throws Exception {
+    public final void isCaptchaValid_EmptyResponse_ShouldReturnMissingResponseMessage() {
         RECAPTCHAManager testInstance = createTestInstance(FAILURE_RESPONSE_JSON);
         assertEquals("Missing reCAPTCHA response token.", testInstance.recaptchaResultString(""));
     }
