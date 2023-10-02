796503e900f1ad79ca60640ce14c66d65e684967
Robert Sayles <robert.sayles@and.digital>
2023-10-02 14:44:44 +0100

    [104]: Cleanup action testing, set some extra configuration values on the retained action

--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacade.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/EventsFacade.java
@@ -1741,8 +1741,4 @@ public class EventsFacade extends AbstractIsaacFacade {
       throw new ClassCastException("The object provided was not an event.");
     }
   }
-
-  private String doThing(int number) {
-    return String.valueOf(number);
-  }
 }
