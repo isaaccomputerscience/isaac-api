693a241a0c7e40b015257eb40a718f7230f7d23d
Robert Sayles <robert.sayles@and.digital>
2023-11-02 13:33:36 +0000

    [175]: Unit tests for new UserAccountManager methods (WIP)

--- src/test/java/uk/ac/cam/cl/dtg/segue/api/managers/UserAccountManagerTest.java
+++ src/test/java/uk/ac/cam/cl/dtg/segue/api/managers/UserAccountManagerTest.java
@@ -2,36 +2,98 @@ package uk.ac.cam.cl.dtg.segue.api.managers;
 
 import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
 import static org.easymock.EasyMock.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static uk.ac.cam.cl.dtg.isaac.dos.users.Role.STUDENT;
+import static uk.ac.cam.cl.dtg.isaac.dos.users.Role.TEACHER;
 import static uk.ac.cam.cl.dtg.segue.api.Constants.HMAC_SALT;
 import static uk.ac.cam.cl.dtg.segue.api.managers.UserAccountManager.isEmailValid;
 import static uk.ac.cam.cl.dtg.segue.api.managers.UserAccountManager.isUserNameValid;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Stream;
 import ma.glasnost.orika.MapperFacade;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.ac.cam.cl.dtg.isaac.dos.PgUserPreferenceManager;
 import uk.ac.cam.cl.dtg.isaac.dos.users.RegisteredUser;
+import uk.ac.cam.cl.dtg.isaac.dos.users.School;
+import uk.ac.cam.cl.dtg.isaac.dto.users.RegisteredUserDTO;
 import uk.ac.cam.cl.dtg.segue.api.Constants;
 import uk.ac.cam.cl.dtg.segue.auth.AuthenticationProvider;
 import uk.ac.cam.cl.dtg.segue.auth.IAuthenticator;
 import uk.ac.cam.cl.dtg.segue.auth.ISecondFactorAuthenticator;
+import uk.ac.cam.cl.dtg.segue.auth.exceptions.MissingRequiredFieldException;
 import uk.ac.cam.cl.dtg.segue.auth.exceptions.NoUserException;
 import uk.ac.cam.cl.dtg.segue.comm.EmailManager;
 import uk.ac.cam.cl.dtg.segue.dao.ILogManager;
 import uk.ac.cam.cl.dtg.segue.dao.SegueDatabaseException;
+import uk.ac.cam.cl.dtg.segue.dao.content.ContentManagerException;
 import uk.ac.cam.cl.dtg.segue.dao.schools.SchoolListReader;
+import uk.ac.cam.cl.dtg.segue.dao.schools.UnableToIndexSchoolsException;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgAnonymousUsers;
 import uk.ac.cam.cl.dtg.segue.dao.users.PgUsers;
 import uk.ac.cam.cl.dtg.util.PropertiesLoader;
 
 public class UserAccountManagerTest {
+
+  PgUsers database;
+  QuestionManager questionmanager;
+  PropertiesLoader propertiesLoader;
+  Map<AuthenticationProvider, IAuthenticator> providersToRegister;
+  MapperFacade dtoMapper;
+  EmailManager emailManager;
+  PgAnonymousUsers pgAnonymousUsers;
+  ILogManager logManager;
+  UserAuthenticationManager userAuthenticationManager;
+  ISecondFactorAuthenticator secondFactorAuthenticator;
+  PgUserPreferenceManager userPreferenceManager;
+  SchoolListReader schoolListReader;
+  UserAccountManager userAccountManager;
+
+  @BeforeEach
+  public void beforeEach() {
+    database = createNiceMock(PgUsers.class);
+    questionmanager = createNiceMock(QuestionManager.class);
+    propertiesLoader = createNiceMock(PropertiesLoader.class);
+    providersToRegister = Map.of();
+    dtoMapper = createNiceMock(MapperFacade.class);
+    emailManager = createNiceMock(EmailManager.class);
+    pgAnonymousUsers = createNiceMock(PgAnonymousUsers.class);
+    logManager = createNiceMock(ILogManager.class);
+    userAuthenticationManager = createNiceMock(UserAuthenticationManager.class);
+    secondFactorAuthenticator = createNiceMock(ISecondFactorAuthenticator.class);
+    userPreferenceManager = createNiceMock(PgUserPreferenceManager.class);
+    schoolListReader = createNiceMock(SchoolListReader.class);
+
+    expect(propertiesLoader.getProperty(HMAC_SALT)).andStubReturn("SALTY");
+    expect(propertiesLoader.getProperty(Constants.SESSION_EXPIRY_SECONDS_DEFAULT)).andStubReturn("60");
+    expect(propertiesLoader.getProperty(Constants.HOST_NAME)).andStubReturn("HOST");
+    expect(propertiesLoader.getProperty(Constants.MAIL_RECEIVERS)).andReturn("admin@localhost");
+    replay(propertiesLoader);
+
+    userAccountManager =
+        new UserAccountManager(database, questionmanager, propertiesLoader, providersToRegister, dtoMapper,
+            emailManager, pgAnonymousUsers, logManager, userAuthenticationManager, secondFactorAuthenticator,
+            userPreferenceManager, schoolListReader);
+  }
+
+  @AfterEach
+  public void afterEach() {
+    reset(database);
+    reset(schoolListReader);
+    reset(emailManager);
+  }
 
   @ParameterizedTest
   @MethodSource("namesToTest")
@@ -113,20 +175,7 @@ public class UserAccountManagerTest {
   }
 
   @Test
-  public void updateTeacherPendingFlag() throws SegueDatabaseException, NoUserException {
-    PgUsers database = createNiceMock(PgUsers.class);
-    QuestionManager questionmanager = createNiceMock(QuestionManager.class);
-    PropertiesLoader propertiesLoader = createNiceMock(PropertiesLoader.class);
-    Map<AuthenticationProvider, IAuthenticator> providersToRegister = Map.of();
-    MapperFacade dtoMapper = createNiceMock(MapperFacade.class);
-    EmailManager emailManager = createNiceMock(EmailManager.class);
-    PgAnonymousUsers pgAnonymousUsers = createNiceMock(PgAnonymousUsers.class);
-    ILogManager logManager = createNiceMock(ILogManager.class);
-    UserAuthenticationManager userAuthenticationManager = createNiceMock(UserAuthenticationManager.class);
-    ISecondFactorAuthenticator secondFactorAuthenticator = createNiceMock(ISecondFactorAuthenticator.class);
-    PgUserPreferenceManager userPreferenceManager = createNiceMock(PgUserPreferenceManager.class);
-    SchoolListReader schoolListReader = createNiceMock(SchoolListReader.class);
-
+  public void updateTeacherPendingFlag_success() throws SegueDatabaseException, NoUserException {
     RegisteredUser initialUserState = new RegisteredUser() {
       {
         setId(1L);
@@ -140,21 +189,80 @@ public class UserAccountManagerTest {
       }
     };
     expect(database.getById(1L)).andReturn(initialUserState);
-    expect(database.createOrUpdateUser(expectedUserState)).andReturn(expectedUserState);
+    expect(database.createOrUpdateUser(expectedUserState)).andStubReturn(expectedUserState);
     replay(database);
-
-    expect(propertiesLoader.getProperty(HMAC_SALT)).andStubReturn("SALTY");
-    expect(propertiesLoader.getProperty(Constants.SESSION_EXPIRY_SECONDS_DEFAULT)).andStubReturn("60");
-    expect(propertiesLoader.getProperty(Constants.HOST_NAME)).andStubReturn("HOST");
-    replay(propertiesLoader);
-
-    UserAccountManager userAccountManager =
-        new UserAccountManager(database, questionmanager, propertiesLoader, providersToRegister, dtoMapper,
-            emailManager, pgAnonymousUsers, logManager, userAuthenticationManager, secondFactorAuthenticator,
-            userPreferenceManager, schoolListReader);
 
     userAccountManager.updateTeacherPendingFlag(1L, true);
 
     verify(database);
+  }
+
+  @Test
+  public void updateTeacherPendingFlag_missingUser() throws SegueDatabaseException {
+    expect(database.getById(1L)).andReturn(null);
+    replay(database);
+
+    assertThrows(NoUserException.class, () -> userAccountManager.updateTeacherPendingFlag(1L, true));
+
+    verify(database);
+  }
+
+  @Test
+  public void sendRoleChangeRequestEmail_success()
+      throws UnableToIndexSchoolsException, ContentManagerException, IOException, SegueDatabaseException,
+      MissingRequiredFieldException {
+
+    School school = new School() {
+      {
+        setUrn("1");
+        setName("SchoolName");
+        setPostcode("Postcode");
+      }
+    };
+    expect(schoolListReader.findSchoolById("1")).andReturn(school);
+    replay(schoolListReader);
+
+    Map<String, Object> expectedEmailDetails = Map.of(
+        "contactGivenName", "GivenName",
+        "contactFamilyName", "FamilyName",
+        "contactUserId", 1L,
+        "contactUserRole", STUDENT,
+        "contactEmail", "test@test.com",
+        "contactSubject", "TEACHER Account Request",
+        "contactMessage", "Hello,\n"
+            + "<br>\n"
+            + "<br>Please could you convert my Isaac account into a teacher account.\n"
+            + "<br>\n"
+            + "<br>My school is: SchoolName, Postcode\n"
+            + "<br>A link to my school website with a staff list showing my name and email (or a phone number to contact the school) is: school staff url\n"
+            + "<br>\n"
+            + "<br>\n"
+            + "<br>Thanks, \n"
+            + "<br>\n"
+            + "<br>GivenName FamilyName",
+        "replyToName", "GivenName FamilyName"
+    );
+    emailManager.sendContactUsFormEmail("admin@localhost", expectedEmailDetails);
+    expectLastCall();
+    replay(emailManager);
+
+    HttpServletRequest request = createNiceMock(HttpServletRequest.class);
+    replay(request);
+    RegisteredUserDTO user = new RegisteredUserDTO() {
+      {
+        setId(1L);
+        setSchoolId("1");
+        setGivenName("GivenName");
+        setFamilyName("FamilyName");
+        setEmail("test@test.com");
+        setRole(STUDENT);
+      }
+    };
+    Map<String, String> requestDetails = Map.of(
+        "verificationDetails", "school staff url",
+        "otherDetails", "more information"
+    );
+
+    userAccountManager.sendRoleChangeRequestEmail(request, user, TEACHER, requestDetails);
   }
 }
