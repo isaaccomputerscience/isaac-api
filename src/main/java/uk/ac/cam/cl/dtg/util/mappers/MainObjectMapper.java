b27a8b20087483c0aec49d3357e149d4f641ab39
Robert Sayles <robert.sayles@and.digital>
2024-02-21 14:32:47 +0000

    [309]: Update name for AudienceContextMapper

--- src/main/java/uk/ac/cam/cl/dtg/util/mappers/MainObjectMapper.java
+++ src/main/java/uk/ac/cam/cl/dtg/util/mappers/MainObjectMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.SubclassExhaustiveStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(subclassExhaustiveStrategy = SubclassExhaustiveStrategy.RUNTIME_EXCEPTION, uses = {
-    MapStructAudienceContextMapper.class, ContentMapperSubclasses.class})
+    AudienceContextMapper.class, ContentMapperSubclasses.class})
 public interface MainObjectMapper
     extends ContentMapper, UserMapper, EventMapper, MiscMapper, QuestionValidationMapper {
   MainObjectMapper INSTANCE = Mappers.getMapper(MainObjectMapper.class);
