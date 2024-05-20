f5dc9a00f56ee1f23c22f6bc2ba098bee44cd266
sxpydo <shavypeynado@gmail.com>
2024-05-20 15:46:38 +0100

    removed Raven enum from the API code

--- src/main/java/uk/ac/cam/cl/dtg/segue/auth/AuthenticationProvider.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/auth/AuthenticationProvider.java
@@ -27,5 +27,5 @@ package uk.ac.cam.cl.dtg.segue.auth;
  * (ignoring case).
  */
 public enum AuthenticationProvider {
-  GOOGLE, FACEBOOK, TWITTER, RAVEN, TEST, SEGUE
+  GOOGLE, FACEBOOK, TWITTER, TEST, SEGUE
 }
