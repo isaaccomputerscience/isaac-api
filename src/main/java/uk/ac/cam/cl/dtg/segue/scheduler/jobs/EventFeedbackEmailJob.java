b8178c6f300d329279070c10ce8a6644e0c95a55
Parth097 <parthsh197@gmail.com>
2025-01-14 14:49:37 +0000

    removing previous non working implementations

--- src/main/java/uk/ac/cam/cl/dtg/segue/scheduler/jobs/EventFeedbackEmailJob.java
+++ src/main/java/uk/ac/cam/cl/dtg/segue/scheduler/jobs/EventFeedbackEmailJob.java
@@ -1,9 +1,6 @@
 package uk.ac.cam.cl.dtg.segue.scheduler.jobs;
 
 import com.google.inject.Injector;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -12,12 +9,9 @@ import org.slf4j.LoggerFactory;
 import uk.ac.cam.cl.dtg.isaac.api.managers.EventNotificationEmailManager;
 import uk.ac.cam.cl.dtg.segue.configuration.SegueGuiceConfigurationModule;
 
-
-
 public class EventFeedbackEmailJob implements Job {
-  private static final Logger log = LoggerFactory.getLogger(EventFeedbackEmailJob.class);
+  private static final Logger log = LoggerFactory.getLogger(EventReminderEmailJob.class);
   private final EventNotificationEmailManager scheduledEmailManager;
-  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
   /**
    * This class is required by quartz and must be executable by any instance of the segue api relying only on the
@@ -30,13 +24,7 @@ public class EventFeedbackEmailJob implements Job {
 
   @Override
   public void execute(final JobExecutionContext context) throws JobExecutionException {
-    scheduler.schedule(() -> {
-      try {
-        scheduledEmailManager.sendFeedbackEmails();
-        log.info("Ran EventFeedbackEmailJob after 24 hours delay");
-      } catch (Exception e) {
-        log.error("Failed to send feedback emails", e);
-      }
-    }, 24, TimeUnit.HOURS);
+    scheduledEmailManager.sendFeedbackEmails();
+    log.info("Ran EventFeedbackEmailJob");
   }
 }
