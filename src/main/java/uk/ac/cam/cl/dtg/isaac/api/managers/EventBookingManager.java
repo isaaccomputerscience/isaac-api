5997c633b074a94f1817e9089c268dc69329f3ce
Parth097 <parthsh197@gmail.com>
2025-03-06 13:26:44 +0000

    reverting comment for PR

--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/EventBookingManager.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/managers/EventBookingManager.java
@@ -549,7 +549,7 @@ public class EventBookingManager {
           EventBookingDTO reservation;
 
           // Set the reservation close date (date at which an unconfirmed reservation is cancelled) to
-          // Set the reservation close date to 72 hours from now, or the event date, which ever is sooner
+          // the day of the event or in EVENT_RESERVATION_CLOSE_INTERVAL_DAYS from now, whichever is earlier.
 
           Instant reservationCloseDate = Collections.min(
               List.of(Instant.now().plus(EVENT_RESERVATION_CLOSE_INTERVAL_DAYS, ChronoUnit.DAYS), event.getDate()));
